00cf9c56154c6ebfe109cbda18a590bb4829be37
Ganesh Kumbhar <140805812+Ganesh-D-Kumbhar@users.noreply.github.com>
2024-09-14 11:02:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+
+
 /*========== menu icon navbar ==========*/
 let mode = document.getElementById('mode');
 let navbar = document.querySelector('.navbar');
@@ -47,54 +48,3 @@ window.onscroll = () => {
 
 };
 
-=======
-/*========== menu icon navbar ==========*/
-let mode = document.getElementById('mode');
-let navbar = document.querySelector('.navbar');
-let body = document.getElementById('bd');
-mode.onclick = () => {
-    if(body.className === "bright"){
-        body.className = 'dark-mode';
-        mode.src = "images/sun.png";
-    }
-  else{
-        body.className = 'bright';
-        mode.src = "images/moon.png";
-    }
-};
-
-
-/*========== scroll sections active link ==========*/
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        };
-    });
-
-
-    /*========== sticky navbar ==========*/
-    let header = document.querySelector('.header');
-
-    header.classList.toggle('sticky', window.scrollY > 100);
-
-
-    /*========== remove menu icon navbar when click navbar link (scroll) ==========*/
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-
-};
-
-
->>>>>>> 547ea3c9bb84fb01f755a6636d473230e9b3d5ea
